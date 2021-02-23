3b9f4a95fe291394d1e605ae7ac2ef826df094d3
OleksandrK-dev <ukorkushko@gmail.com>
2021-02-23 19:14:52 +0200

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
-  return (
-      <div>
-        <Header/>
-        <Technologies/>
-      </div>
-  );
-  /*return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );*/
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <Profile/>
+        </div>
+    );
 }
 
 export default App;
